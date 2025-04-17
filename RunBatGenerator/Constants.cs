c5b71303c1474b8538d0c1dd368adf1929151d70
Alex <xrlogic1@gmail.com>
2025-04-17 17:23:11 +0400

    GRB: Fix

--- RunBatGenerator/Constants.cs
+++ RunBatGenerator/Constants.cs
@@ -3,8 +3,11 @@ namespace SPUtils.RunBatGenerator;
 internal static class Constants
 {
 	public const string BatRelativePath = @"Binaries\Win64";
-	public const string RunServerBatCommand = @"ShadowsPlaygroundServer.exe -log";
-	public const string RunClientBatCommand = @"ShadowsPlaygroundClient.exe -log -windowed -resx=800 -resy=600";
-	public const string RunServerBatFileName = @"!x_run_server.bat";
-	public const string RunClientBatFileName = @"!x_run_client.bat";
+	public const string RunServerBatCommand = "ShadowsPlaygroundServer.exe -log";
+
+	public const string RunClientBatCommand =
+		"ShadowsPlaygroundClient.exe 127.0.0.1 -log -windowed -resx=800 -resy=600";
+
+	public const string RunServerBatFileName = "!x_run_server.bat";
+	public const string RunClientBatFileName = "!x_run_client.bat";
 }
