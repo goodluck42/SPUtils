a11c5c2980c2bc163f11de5652f76830c8398b1f
Alex <xrlogic1@gmail.com>
2025-04-19 08:12:44 +0400

    CMD's are now closed after a process starts

--- RunBatGenerator/Constants.cs
+++ RunBatGenerator/Constants.cs
@@ -3,10 +3,10 @@ namespace SPUtils.RunBatGenerator;
 internal static class Constants
 {
 	public const string BatRelativePath = @"Binaries\Win64";
-	public static string RunServerBatCommand { get; } = $"ShadowsPlaygroundServer.exe -log{Environment.NewLine}exit";
+	public static string RunServerBatCommand { get; } = $"start ShadowsPlaygroundServer.exe -log{Environment.NewLine}exit";
 
 	public static string RunClientBatCommand { get; } =
-		$"ShadowsPlaygroundClient.exe 127.0.0.1 -log -windowed -resx=800 -resy=600{Environment.NewLine}exit";
+		$"start ShadowsPlaygroundClient.exe 127.0.0.1 -log -windowed -resx=800 -resy=600{Environment.NewLine}exit";
 
 	public const string RunServerBatFileName = "!x_run_server.bat";
 	public const string RunClientBatFileName = "!x_run_client.bat";
