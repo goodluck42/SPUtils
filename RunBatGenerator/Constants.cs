8b9c495a2a417b09348a2b72d68278f70c2f4014
Alex <xrlogic1@gmail.com>
2025-08-09 11:07:35 +0400

    Path changes

--- RunBatGenerator/Constants.cs
+++ RunBatGenerator/Constants.cs
@@ -2,16 +2,17 @@ namespace SPUtils.RunBatGenerator;
 
 internal static class Constants
 {
-	public const string BatRelativePath = @"Binaries\Win64";
+	public const string ExePath = @"Binaries\Win64";
+	public const string BatRelativePath = @"";
 
 	public static string RunServerBatCommand { get; } =
-		$"start ShadowsPlaygroundServer.exe -log{Environment.NewLine}exit";
+		$@"start {ExePath}\ShadowsPlaygroundServer.exe -log{Environment.NewLine}exit";
 
 	public static string RunClientBatCommand { get; } =
-		$"start ShadowsPlaygroundClient.exe -log -windowed -resx=800 -resy=600{Environment.NewLine}exit";
+		$@"start {ExePath}\ShadowsPlaygroundClient.exe -log -windowed -resx=800 -resy=600{Environment.NewLine}exit";
 
 	public static string RunClientNoConsoleBatCommand { get; } =
-		$"start ShadowsPlaygroundClient.exe -windowed -resx=800 -resy=600{Environment.NewLine}exit";
+		$@"start {ExePath}\ShadowsPlaygroundClient.exe -windowed -resx=800 -resy=600{Environment.NewLine}exit";
 
 	public const string RunServerBatFileName = "!XRunServer.bat";
 	public const string RunClientBatFileName = "!XRunClient.bat";
