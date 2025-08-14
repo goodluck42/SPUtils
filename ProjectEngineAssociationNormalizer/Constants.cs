64aee4c953e8c5e1792e4ac1ad7bb94dc2db717c
Alex <xrlogic1@gmail.com>
2025-08-14 18:40:37 +0400

    Removed unused consts

--- ProjectEngineAssociationNormalizer/Constants.cs
+++ ProjectEngineAssociationNormalizer/Constants.cs
@@ -8,7 +8,5 @@ internal static class Constants
 	public const string PeanFileName = "pean.sputil";
 	public const string UProjectFileName = "ShadowsPlayground.uproject";
 	public const string UProjectPropertyName = "EngineAssociation";
-	public const string UserIdRegistryPath = @"SOFTWARE\Microsoft\Cryptography";
-	public const string UserIdRegistryKey = "MachineGuid";
 	public const string UEnginesRegistryPath = @"Software\Epic Games\Unreal Engine\Builds";
 }
